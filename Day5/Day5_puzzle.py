67b52ca552bd48d849c3766cebf8efa7ac38dc3e
Abel Guillen <abelazo@gmail.com>
2020-12-05 22:16:13 +0100

    Improves readability

--- Day5/Day5_puzzle.py
+++ Day5/Day5_puzzle.py
@@ -3,16 +3,9 @@ from boardingpass import Pass
 with open('BoardingPasses.txt') as f:
     bps = [Pass(line.strip()) for line in f.readlines()]
 
-max_bp = 0
-for bp in bps:
-    if (bp.get_seat_id() > max_bp):
-        max_bp = bp.get_seat_id()
-
-print("Max Seat ID: {}".format(max_bp))
-
 ids = [bp.get_seat_id() for bp in bps]
 ids.sort()
-
 expected=list(range(ids[0], ids[-1], 1))
 
+print("Max Seat ID: {}".format(ids[-1]))
 print("Empty seats: {}".format(set(ids).symmetric_difference(set(expected))))
