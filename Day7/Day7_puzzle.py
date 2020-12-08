28bf07f8e9afeb8788af8cc0fe0986f5d42281e5
Abel Guillen <abelazo@gmail.com>
2020-12-08 01:46:11 +0100

    Day7 solution

--- Day7/Day7_puzzle.py
+++ Day7/Day7_puzzle.py
@@ -9,7 +9,7 @@ def get_candidates(rules, color):
     return candidates
 
 
-def extract_candidates(rules, color, collector):
+def get_containers(rules, color, collector):
     collector.add(color)
     containers = [rule.get_bag() for rule in get_candidates(rules, color)]
     #print("Containers of {} are {}".format(color, containers))
@@ -17,34 +17,48 @@ def extract_candidates(rules, color, collector):
         return collector
     else:
         for element in containers:
-            #print("Checking {}".format(element))
-            new_containers = extract_candidates(rules, element, collector)
-            #print("New containers: {}".format(new_containers))
+            new_containers = get_containers(rules, element, collector)
+            #print("{} has the these new containers: {}".format(element, new_containers))
             if new_containers is not None:
                 collector = collector.union(set(new_containers))
         return collector
 
+    
 def solve_1(bag_type, rules):
     res = set()
-    containers = extract_candidates(rules, bag_type, res)
-    containers.remove(bag_type) # This could be improved
-    print("The number of bags that can contain '{}' is {}: \n{}".format(bag_type, len(containers), containers))
+    containers = get_containers(rules, bag_type, res)
+    containers.remove(bag_type) # TODO: This could be improved
+    print("The number of bags that can contain '{}' is {}".format(bag_type, len(containers)))
 
+
+def get_num_bags(num_bags, rule, rules):
+    #print ("Evaluating {}".format(rule))
+    content = rule.get_content()
+    
+    if content is None or content == {}:
+        return num_bags
+    else:
+        cumul = num_bags
+        for element in content:
+            num_contents = int(content[element])
+            jander = (num_bags * num_contents)
+            element_rule = [item for item in rules if item.get_bag() == element][0]
+            cumul += get_num_bags(jander, element_rule, rules)
+            #print ("Value of cumul afer processing {}: {}".format(element, cumul))
+        #print ("Value of cumul {}".format(cumul))
+        return cumul
+
+def solve_2(bag_type, rules):
+    shiny_gold_rule = [item for item in rules if item.get_bag() == bag_type][0]
+    contained_bags = get_num_bags(1, shiny_gold_rule, rules) - 1
+    print("The number of bags within a '{}' is {}".format(bag_type, contained_bags))
+    
+    
 if __name__ == '__main__':
     with open('Rules.txt') as f:
         all_rules = [Rule(line.strip()) for line in f.readlines()]
 
     bag_type = "shiny gold"
-    solve_1(bag_type, all_rules)
 
-# if __name__ == '__other__':
-#     other_rules = [Rule("light red bags contain 1 bright white bag, 2 muted yellow bags."),
-#                    Rule("dark orange bags contain 3 bright white bags, 4 muted yellow bags."),
-#                    Rule("bright white bags contain 1 shiny gold bag."),
-#                    Rule("muted yellow bags contain 2 shiny gold bags, 9 faded blue bags."),
-#                    Rule("shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags."),
-#                    Rule("dark olive bags contain 3 faded blue bags, 4 dotted black bags."),
-#                    Rule("vibrant plum bags contain 5 faded blue bags, 6 dotted black bags."),
-#                    Rule("faded blue bags contain no other bags."), Rule("dotted black bags contain no other bags.")]
-#     start = "shiny gold"
-#     print("The number of bags that can contain {} is {}".format(start, len(extract_candidates(other_rules, start, res))))
+    solve_1(bag_type, all_rules)
+    solve_2(bag_type, all_rules)
--- Day7/rules.py
+++ Day7/rules.py
@@ -3,9 +3,15 @@ def _get_color(definition):
 
 
 def _get_content(definition):
-    return [x.strip().replace(" bags", "").replace(" bag", "") for x in
+    raw_content = [x.strip().replace(" bags", "").replace(" bag", "") for x in
             definition.split("contain")[1].strip()[0:-1].split(',')]
-
+    content = {}
+    for bag in raw_content:
+        if bag != "no other":
+            amount = bag.split(' ')[0].strip()
+            color = bag.split(' ',1)[1].strip()
+            content[color] = amount
+    return content
 
 class Rule:
     def __init__(self, definition):
@@ -28,3 +34,13 @@ class Rule:
         return any([color in c for c in self._content])
 
 
+if __name__ == '__main__':
+     other_rules = [Rule("light red bags contain 1 bright white bag, 2 muted yellow bags."),
+                    Rule("dark orange bags contain 3 bright white bags, 4 muted yellow bags."),
+                    Rule("bright white bags contain 1 shiny gold bag."),
+                    Rule("muted yellow bags contain 2 shiny gold bags, 9 faded blue bags."),
+                    Rule("shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags."),
+                    Rule("dark olive bags contain 3 faded blue bags, 4 dotted black bags."),
+                    Rule("vibrant plum bags contain 5 faded blue bags, 6 dotted black bags."),
+                    Rule("faded blue bags contain no other bags."), Rule("dotted black bags contain no other bags.")]
+     print(other_rules)
