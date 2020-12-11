37b6fc4614ccbb5210fe1355a00f1f3555e47359
Abel Guillen <abelazo@gmail.com>
2020-12-11 19:20:07 +0100

    Day8 scafolding

---
+++ Day11/solution.py
@@ -0,0 +1,12 @@
+
+def solve_1(adapters):
+    pass
+
+def solve_2(adapters):
+    pass
+
+if __name__ == '__main__':
+    with open('Seats.txt') as f:
+        seats = [(line.strip()) for line in f.readlines()]
+
+    print (seats)
