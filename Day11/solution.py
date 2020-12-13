038134dc66ecf72c86010672b6f495c8f80aea44
Abel Guillen <abelazo@gmail.com>
2020-12-13 23:01:46 +0100

    Solves puzle 1

--- Day11/solution.py
+++ Day11/solution.py
@@ -1,12 +1,125 @@
 
-def solve_1(adapters):
-    pass
 
-def solve_2(adapters):
-    pass
+class Point:
+    FLOOR = "."
+    EMPTY = "L"
+    OCCUPIED = "#"
+
+    def __init__(self, x, y, status):
+        self._x = x
+        self._y = y
+        self._status = status
+        self._context = []
+
+    def __str__(self):
+        #return "s:{}|c:{}".format(self._status, len(self._context))
+        return "{}".format(self._status)
+
+    def __repr__(self):
+        #return "s:{}|c:{}".format(self._status, len(self._context))
+        return "{}".format(self._status)
+    
+    def set_context(self, context):
+        self._context = context.copy()
+
+    def get_status(self):
+        return str(self._status)
+
+    def new_status(self):
+        num_occupied_seats = len([s for s in self._context if s.get_status() == Point.OCCUPIED])
+        if self.get_status() == Point.EMPTY and num_occupied_seats == 0:
+            return Point.OCCUPIED
+        elif(self.get_status() == Point.OCCUPIED and num_occupied_seats >= 4):
+            return Point.EMPTY
+        else:
+            return self.get_status()
+
+def generate_points(seats):
+    ret = []
+    for r in range(len(seats)):
+        ret.append([])
+        row = seats[r]
+        for c in range(len(row)):
+            status = row[c]
+            p = Point(r, c, status)
+            ret[r].append(p)
+    return ret
+
+
+def get_context(points, r, c):
+    context = []
+    # previous row
+    if r-1 >= 0:
+        prev_row = points[r-1]
+        if c-1 >= 0: context.append(prev_row[c-1])
+        context.append(prev_row[c])
+        if c+1 < len(prev_row): context.append(prev_row[c+1])
+
+    # current row
+    curr_row = points[r]
+    if c-1 >= 0: context.append(curr_row[c-1])
+    # context.append(curr_row[c])
+    if c+1 < len(curr_row): context.append(curr_row[c+1])
+
+    # next row
+    if r+1 < len(points):
+        next_row = points[r+1]
+        if c-1 >= 0: context.append(next_row[c-1])
+        context.append(next_row[c])
+        if c+1 < len(next_row): context.append(next_row[c+1])
+
+    return context
+    
+def initialize_contexts(points):
+    for r in range(len(points)):
+        for c in range(len(points[r])):
+            context = get_context(points, r, c)
+            points[r][c].set_context(context)
+
+
+def apply_rules(points):
+    new_points = []
+    for r in range(len(points)):
+        new_points.append([])
+        for c in range(len(points[r])):
+            p = Point(r,c, points[r][c].new_status())
+            new_points[r].append(p)
+    return new_points
+
+
+def equals(state1, state2):
+    equals = True
+    for r in range(len(state1)):
+        for c in range(len(state1[r])):
+            equals = equals and state1[r][c].get_status() == state2[r][c].get_status()
+    return equals
+
+def solve_1(seats):
+    points = generate_points(seats)
+    initialize_contexts(points)
+
+    curr_points = points
+    new_points = []
+    isDifferent = True
+    while isDifferent:
+        new_points = apply_rules(curr_points)
+        initialize_contexts(new_points)
+        isDifferent = not equals(curr_points, new_points)
+        curr_points = new_points
+
+    counter = 0
+    for r in range(len(curr_points)):
+        for c in range(len(curr_points[r])):
+            if (curr_points[r][c].get_status() == Point.OCCUPIED): counter += 1
+    return counter
+
+def solve_2(seats):
+    return None
+
 
 if __name__ == '__main__':
     with open('Seats.txt') as f:
         seats = [(line.strip()) for line in f.readlines()]
 
-    print (seats)
+    print("Solution to 1 is {}".format(solve_1(seats)))
+    print("Solution to 2 is {}".format(solve_2(seats)))
--- Day11/test.py
+++ Day11/test.py
@@ -1,26 +1,165 @@
 import unittest
 
+import solution
+from solution import Point
+
+no_occupied_context = [Point.FLOOR, Point.FLOOR, Point.FLOOR,
+                       Point.EMPTY,              Point.EMPTY,
+                       Point.EMPTY, Point.EMPTY, Point.FLOOR]
+
+three_occupied_context = [Point.OCCUPIED, Point.FLOOR, Point.FLOOR,
+                          Point.OCCUPIED,              Point.EMPTY,
+                          Point.OCCUPIED, Point.FLOOR, Point.FLOOR]
+
+four_occupied_context = [Point.OCCUPIED, Point.FLOOR, Point.OCCUPIED,
+                         Point.OCCUPIED,              Point.EMPTY,
+                         Point.OCCUPIED, Point.FLOOR, Point.FLOOR]
+
+example = ["L.LL.LL.LL",
+           "LLLLLLL.LL",
+           "L.L.L..L..",
+           "LLLL.LL.LL",
+           "L.LL.LL.LL",
+           "L.LLLLL.LL",
+           "..L.L.....",
+           "LLLLLLLLLL",
+           "L.LLLLLL.L",
+           "L.LLLLL.LL"]
+
 class TestClass(unittest.TestCase):
 
-    def _test(self):
+    def test_get_status(self):
         # GIVEN
+        context = [Point(1, x, '.') for x in range(8)]
+        expected_status = Point.EMPTY
+        p = Point(1, 2, expected_status)
+        p.set_context(context)
+
         # WHEN
+        status = p.get_status()
+
         # THEN
-        pass
+        self.assertEqual(status, expected_status)
 
-    def test_one(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    def test_floor_should_never_switch(self):
+        # GIVEN
+        context = [Point(1, x, '.') for x in range(8)]
+        p = Point(1, 2, Point.FLOOR)
+        p.set_context(context)
 
-    def test_two(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        # WHEN
+        status = p.new_status()
 
-    def test_three(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
-        # check that s.split fails when the separator is not a string
-        with self.assertRaises(TypeError):
-            s.split(2)
+        # THEN
+        self.assertEqual(status, Point.FLOOR)
+
+    def test_empty_should_switch_to_occupied(self):
+        # GIVEN
+        context = [Point(1, 1, x) for x in no_occupied_context]
+        p = Point(1, 2, Point.EMPTY)
+        p.set_context(context)
+
+        # WHEN
+        status = p.new_status()
+
+        # THEN
+        self.assertEqual(status, Point.OCCUPIED)
+
+    def test_empty_should_not_switch_to_occupied(self):
+        # GIVEN
+        context = [Point(1, 1, x) for x in three_occupied_context]
+        p = Point(1, 2, Point.EMPTY)
+        p.set_context(context)
+
+        # WHEN
+        status = p.new_status()
+
+        # THEN
+        self.assertEqual(status, Point.EMPTY)
+        
+    def test_occupied_should_switch_to_empty(self):
+        # GIVEN
+        context = [Point(1, 1, x) for x in four_occupied_context]
+        p = Point(1, 2, Point.OCCUPIED)
+        p.set_context(context)
+
+        # WHEN
+        status = p.new_status()
+
+        # THEN
+        self.assertEqual(status, Point.EMPTY)
+
+    def test_occupied_should_not_switch_to_empty(self):
+        # GIVEN
+        context = [Point(1, 1, x) for x in three_occupied_context]
+        p = Point(1, 2, Point.OCCUPIED)
+        p.set_context(context)
+
+        # WHEN
+        status = p.new_status()
+
+        # THEN
+        self.assertEqual(status, Point.OCCUPIED)
+
+
+    def test_generate_points(self):
+        # GIVEN
+
+        # WHEN
+        points = solution.generate_points(example)
+
+        # THEN
+        for r in range(len(example)):
+            for c in range(len(example[r])):
+                points[r][c].get_status() == example[r][c]
+
+    def test_get_context(self):
+        # GIVEN
+        points = solution.generate_points(example)
+
+        # WHEN
+        context = solution.get_context(points, 1, 1)
+
+        # THEN
+        expected_context = [Point.EMPTY, Point.FLOOR, Point.EMPTY, Point.EMPTY, Point.EMPTY, Point.EMPTY, Point.FLOOR, Point.EMPTY]
+        for i in range(len(context)):
+            self.assertEqual(context[i].get_status(), expected_context[i])
+
+    def test_get_upper_limit_context(self):
+        # GIVEN
+        points = solution.generate_points(example)
+
+        # WHEN
+        context = solution.get_context(points, 0, 0)
+
+        # THEN
+        expected_context = [Point.FLOOR, Point.EMPTY, Point.EMPTY]
+        for i in range(len(context)):
+            self.assertEqual(context[i].get_status(), expected_context[i])
+
+
+    def test_get_lower_limit_context(self):
+        # GIVEN
+        points = solution.generate_points(example)
+
+        # WHEN
+        context = solution.get_context(points, 9, 9)
+
+        # THEN
+        expected_context = [Point.FLOOR, Point.EMPTY, Point.EMPTY]
+        for i in range(len(context)):
+            self.assertEqual(context[i].get_status(), expected_context[i])
+
+
+    def test_solution1(self):
+        # GIVEN
+        points = solution.generate_points(example)
+
+        # WHEN
+        seats = solution.solve_1(points)
+
+        # THEN
+        self.assertEqual(seats, 37)
 
 if __name__ == '__main__':
     unittest.main()
